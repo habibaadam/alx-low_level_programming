a3cd8477921c7d6d1dedbabd7f03d3f6c756ac02
habibaadam <habiepalmer@gmail.com>
2023-04-21 14:15:08 -0700

    final task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,19 @@
   */
 int main(void)
 {
-	int l, h;
+	int h, q;
 
-	for (l = 0; h <= 98; l++)
+	for (h = 0; h <= 98; h++)
 	{
 
-	for (h = l + 1; h <= 99; h++)
+	for (q = h + 1; q <= 99; q++)
 	{
-	putchar((l / 10) + '0');
-	putchar((l % 10) + '0');
-	putchar(' ');
 	putchar((h / 10) + '0');
 	putchar((h % 10) + '0');
-	if (l == 98 && h == 99)
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (h == 98 && q == 99)
 	continue;
 	putchar(',');
 	putchar(' ');
