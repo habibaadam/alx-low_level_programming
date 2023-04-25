12c901d40cde47f2aa4ec62b1668242ffd398924
habibaadam <habiepalmer@gmail.com>
2023-04-25 09:15:33 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add - Entry point
+  * Description - 'a function that adds two integers and prints the results'
+  * @h1: first number to be computed
+  * @h2: second number to be computed
+  * Return: integer value or results
+  */
+
+int add(int h1, int h2)
+{
+	return (h1 + h2);
+}
