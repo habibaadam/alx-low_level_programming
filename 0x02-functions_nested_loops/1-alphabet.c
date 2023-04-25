e210eabf691f389867ce92b707c1bd6787baa47f
habibaadam <habiepalmer@gmail.com>
2023-04-25 04:20:31 -0700

    task0,task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints alphabets in lowercase
+  *
+  * Return: void (sucess)
+  */
+
+void print_alphabet(void)
+{
+	int h;
+
+	for (h = 97; h <= 122; h++)
+	{
+	_putchar(h);
+	}
+	_putchar('\n');
+}
