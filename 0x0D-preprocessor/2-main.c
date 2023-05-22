512cfc1cbd5d75fa8d672a631737215437a3a1e8
habibaadam <habiepalmer@gmail.com>
2023-05-22 02:54:17 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char filename[10] = "2-main.c";
+
+	printf("%s\n", filename);
+
+	return (0);
+}
