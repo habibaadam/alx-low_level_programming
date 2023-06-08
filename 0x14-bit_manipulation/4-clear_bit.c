8713c7491d4e6de024985ed9c5220d362bb27200
habibaadam <habiepalmer@gmail.com>
2023-06-08 07:01:34 -0700

    fixed clearing of bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = (~(1UL << index) & *n);
+	*n &= ~(1 << index);
 
-	return (-1);
+	return (1);
 }
