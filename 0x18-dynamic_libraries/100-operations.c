15912ce83f5118eedf2e25e3b92386c2ecbcb9c7
habibaadam <habiepalmer@gmail.com>
2023-08-21 05:26:49 -0700

    C functions from Python

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -10,22 +10,21 @@ int sub(int a, int b)
 	return (a - b);
 }
 
-int div(int a, int b)
-{
-	return (a / b );
-}
-
 int mul(int a, int b)
 {
 	return (a * b);
 }
 
+int div(int a, int b)
+{
+	return (a / b);
+}
+
 int mod(int a, int b)
 {
-	if (b != 0)
+	if(b == 0)
 	{
 		return (0);
 	}
-
 	return (a % b);
 }
