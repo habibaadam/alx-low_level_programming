09aa9bae662977d5895477c01c09ea349c7fb240
habibaadam <habiepalmer@gmail.com>
2023-09-07 22:12:59 +0000

    3. >>> ht[betty] = cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,7 +3,7 @@
 /**
 * hash_table_set - function for updating key or value of a hash table,
 * In case of collision, add the new node at the beginning of the list
-
+*
 * @ht: a pointer to the hash table
 * @key: key of the hashtable, key can not be an empty string
 * @value: value associated with the key, value must be duplicated,
@@ -19,11 +19,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx, h;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	    return (0);
+		return (0);
 
 	val_copy = strdup(value);
 	if (val_copy == NULL)
-	    return (0);
+		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
 	/*updates in case there is already an existing array at spot for insertion*/
