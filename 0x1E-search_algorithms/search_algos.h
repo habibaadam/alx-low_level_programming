d1f4bdcc9672b314b238e3de52b52fc0061e9423
habibaadam <habiepalmer@gmail.com>
2023-12-12 13:31:17 +0000

    11. Advanced binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -46,6 +46,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
+int rec_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
