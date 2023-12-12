88f66cb2dbbfcd2d46807442e1f308e3b9e91810
habibaadam <habiepalmer@gmail.com>
2023-12-12 12:15:23 +0000

    7. Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,6 +13,9 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t i, jump, step;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
 	step = sqrt(size);
 	for (i = jump = 0; jump < size && array[jump] < value;)
 	{
@@ -29,5 +32,4 @@ int jump_search(int *array, size_t size, int value)
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 	return (array[i] == value ? (int)i : -1);
-
 }
